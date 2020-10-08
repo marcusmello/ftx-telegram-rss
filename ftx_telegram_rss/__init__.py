0fb0a919e407276f749ded657eba4ababcc75410
marcusmello <mandealista@gmail.com>
2020-10-08 01:25:41 -0300

    First code lines: getting 'instruments/futures' names list

--- ftx_telegram_rss/__init__.py
+++ ftx_telegram_rss/__init__.py
@@ -1,1 +1,20 @@
 __version__ = '0.1.0'
+
+import requests
+
+def get_response(endpoint: str):
+    with requests.get(endpoint) as response:
+        if response.status_code == 200:
+            return response
+
+
+def get_instruments_names_list():
+
+    endpoint = "https://ftx.com/api/futures"
+
+    instruments = (get_response(endpoint)).json()
+
+    return [
+        instrument['name'] for
+        instrument in instruments['result']
+    ]
