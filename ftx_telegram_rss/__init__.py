3a46d270a9825ea2f11d5f490a05b6a492ab37d1
marcusmello <mandealista@gmail.com>
2020-10-09 13:44:00 -0300

    Added 'slots' and attributes made private on 'Future' | Input list handled | New method '_unsorted_filtered_funding_rate_list' replacing '_original_funding_rate_list' and '_future' in order to comply 'OUTPUT_THRESHOLD' requirement | '_sleep' method on 'CheckAndReport' in order to discount the time spent processing the current step

--- ftx_telegram_rss/__init__.py
+++ ftx_telegram_rss/__init__.py
@@ -43,50 +43,73 @@ class RichList:
 
 
 class Futures:
+    __slots__ = [
+        "_funding_rate_key",
+        "_listed_futures_endpoint",
+        "_future_detail_endpoint",
+        "_output_number",
+        "_output_treshold",
+        "_futures_names",
+    ]
+
     def __init__(self):
         self._funding_rate_key = "nextFundingRate"
-        self.listed_futures_endpoint = "https://ftx.com/api/futures"
-        self.future_detail_endpoint = "https://ftx.com/api/futures/{}/stats"
-        self.output_number = env.int("OUTPUT_NUMBER")
-        self.output_treshold = env.float("OUTPUT_THRESHOLD")
+        self._listed_futures_endpoint = "https://ftx.com/api/futures"
+        self._future_detail_endpoint = "https://ftx.com/api/futures/{}/stats"
+        self._output_number = env.int("OUTPUT_NUMBER")
+        self._output_treshold = env.float("OUTPUT_THRESHOLD")
         self._get_futures_names()
 
     def _get_futures_names(self):
+        """The sanitization of the list of names is dispensed here, Since
+        this should be a simple script that solves an exercise. The user
+        is expected to list existing FTX futures names; otherwise,
+        nonexistent names will be ignored, due to the "try/catch" in
+        '_unsorted_filtered_funding_rate_list'. So, as long as there is
+        a few correct names in the list, it will run smoothly.
+        """
+
         _input_list = env.list("LIST_OF_FUTURES")
-        self._futures_names = self.get_all_listed_futures_names()
+
+        self._futures_names = (
+            self.get_all_listed_futures_names()
+            if _input_list == ["all"]
+            else _input_list
+        )
 
     def get_all_listed_futures_names(self) -> list:
-        futures = (get_response(self.listed_futures_endpoint)).json()
+        futures = (get_response(self._listed_futures_endpoint)).json()
         return [future["name"] for future in futures["result"]]
 
-    def _future(self, name):
-        future_dict = (
-            get_response(self.future_detail_endpoint.format(name))
-        ).json()
-
-        funding_rate = future_dict["result"][self._funding_rate_key]
-        return (name, funding_rate)
-
-    def _original_funding_rate_list(self):
+    def _unsorted_filtered_funding_rate_list(self) -> list:
         funding_rate_list = []
 
         for name in self._futures_names:
             try:
-                funding_rate_list.append(self._future(name))
+                future_dict = (
+                    get_response(self._future_detail_endpoint.format(name))
+                ).json()
+
+                funding_rate = float(
+                    future_dict["result"][self._funding_rate_key]
+                )
+
+                if funding_rate >= self._output_treshold:
+                    funding_rate_list.append((name, funding_rate))
 
             except:  # The notable exception here is a nonexistent
                 pass  # 'nextFundingRate' key in 'future_dict["result"]'
 
         return funding_rate_list
 
-    def funding_rate_list(self):
+    def funding_rate_top_bottom_rich_list(self) -> RichList:
         rich_list = RichList()
 
-        raw_list = self._original_funding_rate_list()
+        raw_list = self._unsorted_filtered_funding_rate_list()
         _list = sorted(raw_list, key=lambda future: future[1], reverse=True)
 
-        n_top = n_bottom = self.output_number
-        if len(_list) < self.output_number:
+        n_top = n_bottom = self._output_number
+        if len(_list) < self._output_number:
             n_top = int(len(_list) / 2)
             n_bottom = len(_list) - n_top
 
@@ -107,6 +130,7 @@ class TelegramReport:
 
 class CheckAndReport:
     def __init__(self) -> None:
+        self._process_time_seconds = 0
         self.msg = None
         self.futures = Futures()
 
@@ -118,6 +142,14 @@ class CheckAndReport:
         except:
             self.telegram_report = None
 
+    def _sleep(self):
+        _sleep_time = (
+            env.int("UPDATE_DELAY") - self._process_time_seconds
+            if env.int("UPDATE_DELAY") > self._process_time_seconds
+            else env.int("UPDATE_DELAY")
+        )
+        time.sleep(_sleep_time)
+
     def do_report(self, msg):
         print(msg)
         if self.telegram_report:
@@ -125,11 +157,10 @@ class CheckAndReport:
 
     def run(self):
         while True:
-            futures_funding_rate_list = self.futures.funding_rate_list()
+            start_time = pendulum.now()
+            futures_funding_rate_list = (
+                self.futures.funding_rate_top_bottom_rich_list()
+            )
             self.do_report(futures_funding_rate_list.as_formatted_text())
-            time.sleep(env.int("UPDATE_DELAY"))
-
-
-if __name__ == "__main__":
-    check_and_report = CheckAndReport()
-    check_and_report.run()
+            self._process_time_seconds = (pendulum.now() - start_time).seconds
+            self._sleep()
